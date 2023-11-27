6d0fc203acdbfc42af9b98c433890ea4aa74bf5f
yusuf karslı <yusufkarsli@outlook.com>
2023-11-27 20:18:19 +0300

    menu logic update

---
+++ KaleGroup/KaleGroup.Business/Business/MenuLogic.cs
@@ -0,0 +1,46 @@
+﻿using KaleGroup.Business.Dto;
+using KaleGroup.Business.IBusiness;
+using KaleGroup.Common.Helper;
+using KaleGroup.Data.Entities;
+using KaleGroup.DataAccess.Repository.SubMenu.Interface;
+using KaleGroup.DataAccess.Repository.User.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaleGroup.Business.Business
+{
+    public class MenuLogic : IMenuLogic
+    {
+        private readonly IMenuRepository _menuRepository;
+
+        public MenuLogic(IMenuRepository menuRepository)
+        {
+            _menuRepository = menuRepository;
+        }
+
+        public List<MenuDtos> GetMenuDtos()
+        {
+            List<MenuDtos> menuList = new List<MenuDtos>();
+            var menuResult = _menuRepository.GetMenuList();
+            foreach ( var item in menuResult)
+            {
+                MenuDtos menu = new MenuDtos();
+                menu.Name= item.Name;
+                menu.Description= item.Description;
+                menu.EnDescription= item.EnDescription;
+                menu.EnName= item.EnName;
+                menu.Id= item.Id;
+                menuList.Add(menu); 
+
+            }
+            return menuList;
+        }
+    }
+}
+
+
+ 
+ 
---
+++ KaleGroup/KaleGroup.Business/IBusiness/IMenuLogic.cs
@@ -0,0 +1,18 @@
+﻿
+using KaleGroup.Business.Dto;
+using KaleGroup.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaleGroup.Business.IBusiness
+{
+    public interface IMenuLogic
+    {
+       List<MenuDtos> GetMenuDtos();
+      
+    }
+}
